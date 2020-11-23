5245125c06859b56a4c5aab7951bf8d6d4afedd7
Duong Le Bac <lebac1010@gmail.com>
2020-11-23 20:49:54 +0700

    Update

--- Objective-C/TOCropViewController/TOCropViewController.m
+++ Objective-C/TOCropViewController/TOCropViewController.m
@@ -576,8 +576,8 @@
     //Prepare the list that will be fed to the alert view/controller
     
     // Ratio titles according to the order of enum TOCropViewControllerAspectRatioPreset
-    NSArray<NSString *> *portraitRatioTitles = @[originalButtonTitle, squareButtonTitle, @"2:3", @"3:5", @"3:4", @"4:5", @"5:7", @"9:16"];
-    NSArray<NSString *> *landscapeRatioTitles = @[originalButtonTitle, squareButtonTitle, @"3:2", @"5:3", @"4:3", @"5:4", @"7:5", @"16:9"];
+    NSArray<NSString *> *portraitRatioTitles = @[originalButtonTitle, squareButtonTitle, @"5:4", @"4:3", @"3:2", @"16:9", @"9:16", @"2:3", @"3:4", @"4:5"];
+    NSArray<NSString *> *landscapeRatioTitles = @[originalButtonTitle, squareButtonTitle, @"5:4", @"4:3", @"3:2", @"16:9", @"9:16", @"2:3", @"3:4", @"4:5"];
 
     NSMutableArray *ratioValues = [NSMutableArray array];
     NSMutableArray *itemStrings = [NSMutableArray array];
@@ -630,8 +630,8 @@
     //Prepare the list that will be fed to the alert view/controller
     
     // Ratio titles according to the order of enum TOCropViewControllerAspectRatioPreset
-    NSArray<NSString *> *portraitRatioTitles = @[originalButtonTitle, squareButtonTitle, @"2:3", @"3:5", @"3:4", @"4:5", @"5:7", @"9:16"];
-    NSArray<NSString *> *landscapeRatioTitles = @[originalButtonTitle, squareButtonTitle, @"3:2", @"5:3", @"4:3", @"5:4", @"7:5", @"16:9"];
+    NSArray<NSString *> *portraitRatioTitles = @[originalButtonTitle, squareButtonTitle, @"5:4", @"4:3", @"3:2", @"16:9", @"9:16", @"2:3", @"3:4", @"4:5"];
+    NSArray<NSString *> *landscapeRatioTitles = @[originalButtonTitle, squareButtonTitle, @"5:4", @"4:3", @"3:2", @"16:9", @"9:16", @"2:3", @"3:4", @"4:5"];
 
     NSMutableArray *ratioValues = [NSMutableArray array];
     NSMutableArray *itemStrings = [NSMutableArray array];
